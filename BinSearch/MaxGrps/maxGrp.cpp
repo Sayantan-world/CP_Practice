a5ffaa48ede70dca4eb9156e67d9f7fcf50e14e8
Sayantan Pal <sayantan.world98@gmail.com>
2020-01-11 20:33:43 +0530

    Max grps

---
+++ BinSearch/MaxGrps/maxGrp.cpp
@@ -0,0 +1,49 @@
+/*
+ * AUTHOR : Sayanatan Pal
+ * DATE : 1.01.2020
+ * Time Complexity --> O(log(maxGrp) * n)
+ * Queston: Given a group of animals in various boxes, calculate max number of packets u can make with K distinct animals in each packet.
+ * Ex: A box has some animals(elements in list denote the number of animals in i th box) = [1,2,3,4,5,6,7] K=5 , Answer = 5 packets
+ * Binary Search Implementation
+ */
+using namespace std;
+
+const int Nmax = 100005;
+
+bool isValid(int a[], int n, int k, long long x) {
+        long long cnt = 0;
+        for(int i = 1; i <= n; i++)
+                cnt += min(x, 1LL * a[i]);
+        return cnt >= 1LL * x * k;
+}
+
+long long sum_of(int a[], int n) {
+        long long sum = 0;
+        for(int i = 1; i <= n; i++)
+                sum += a[i];
+        return sum;
+}
+
+long long bSearch(int a[], int n, int k) {
+        long long Left = 0, Right = sum_of(a, n), ans;
+        while(Left <= Right) {
+                long long Mid = (Left + Right) / 2;
+                if(isValid(a, n, k, Mid)) {
+                        ans = Mid;
+                        Left = Mid + 1;
+                }
+                else
+                        Right = Mid - 1;
+        }
+        return ans;
+}
+
+int main()
+{
+        int n, k, a[Nmax];
+        cin >> n >> k;
+        for(int i = 1; i <= n; i++)
+                cin >> a[i];
+        cout << bSearch(a, n, k);
+        return 0;
+}
